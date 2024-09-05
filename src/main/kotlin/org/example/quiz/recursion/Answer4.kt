d3e3b56c18530b3cf9e2815123f40df3e432208d
YoungJun-L <dudwns328@naver.com>
2024-09-05 22:30:50 +0900

    4장 추가

--- src/main/kotlin/org/example/quiz/recursion/Answer4.kt
+++ src/main/kotlin/org/example/quiz/recursion/Answer4.kt
@@ -94,7 +94,104 @@ class Answer4 {
         else
             f(list.head(), foldRight(list.tail(), identity, f))
 
-    private fun prepend(c: Char, s: String) = "$c%s"
+    private fun prepend(c: Char, s: String) = "$c$s"
 
     private fun toStringV3(list: List<Char>): String = foldRight(list, "") { c, s -> prepend(c, s) }
+
+    fun <T> reverse(list: List<T>): List<T> = foldLeft(list, mutableListOf(), ::prepend)
+
+    private fun <T> prepend(list: List<T>, element: T): List<T> = listOf(element) + list
+
+    fun <T> reverseV2(list: List<T>): List<T> = foldLeft(list, listOf(), ::prependV2)
+
+    private fun <T> prependV2(list: List<T>, element: T): List<T> =
+        foldLeft(list, listOf(element)) { lst, e -> lst + e }
+
+    fun range(start: Int, endExclusive: Int): List<Int> {
+        val list = mutableListOf<Int>()
+        var index = start
+        while (index < endExclusive) {
+            list.add(index)
+            index++
+        }
+        return list
+    }
+
+    fun <T> unfold(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> {
+        val list = mutableListOf<T>()
+        var elem = seed
+        while (p(elem)) {
+            list.add(elem)
+            elem = f(elem)
+        }
+        return list
+    }
+
+    fun rangeV2(start: Int, endExclusive: Int): List<Int> = unfold(start, { x -> x + 1 }, { x -> x < endExclusive })
+
+    fun rangeV3(start: Int, endExclusive: Int): List<Int> =
+        if (endExclusive <= start)
+            listOf()
+        else
+            prependV2(rangeV3(start + 1, endExclusive), start)
+
+    fun <T> unfoldV2(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> =
+        if (!p(seed))
+            listOf()
+        else
+            prependV2(unfoldV2(f(seed), f, p), seed)
+
+    fun <T> unfoldV3(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> {
+        tailrec fun unfold(acc: List<T>, seed: T): List<T> =
+            if (p(seed))
+                unfold(acc + seed, f(seed))
+            else
+                acc
+        return unfold(listOf(), seed)
+    }
+
+    fun fibo(n: Int): String {
+        tailrec fun fibo(
+            acc: List<BigInteger>,
+            acc1: BigInteger,
+            acc2: BigInteger,
+            x: BigInteger
+        ): List<BigInteger> = when (x) {
+            BigInteger.ZERO -> acc
+            else -> fibo(acc + (acc1 + acc2), acc1 + acc2, acc1, x - BigInteger.ONE)
+        }
+        return fibo(
+            listOf(BigInteger.ZERO),
+            BigInteger.ZERO,
+            BigInteger.ONE,
+            BigInteger.valueOf(n.toLong())
+        ).joinToString()
+    }
+
+    fun <T> iterate(seed: T, f: (T) -> T, n: Int): List<T> {
+        tailrec fun iterate(acc: List<T>, seed: T): List<T> =
+            if (acc.size == n)
+                acc
+            else
+                iterate(acc + seed, f(seed))
+        return iterate(listOf(), seed)
+    }
+
+    fun <T, U> mapV1(list: List<T>, f: (T) -> U): List<U> {
+        tailrec fun map(acc: List<U>, list: List<T>): List<U> =
+            if (list.isEmpty())
+                acc
+            else
+                map(acc + f(list.head()), list.tail())
+        return map(listOf(), list)
+    }
+
+    fun <T, U> mapV2(list: List<T>, f: (T) -> U): List<U> = foldLeft(list, listOf()) { lst, e -> lst + f(e) }
+
+    fun fiboCorecursive(n: Int): String {
+        val seed = BigInteger.ZERO to BigInteger.ONE
+        val f = { x: Pair<BigInteger, BigInteger> -> (x.first + x.second) to x.first }
+        val list = iterate(seed, f, n + 1)
+        return mapV2(list) { it.first }.joinToString()
+    }
 }
