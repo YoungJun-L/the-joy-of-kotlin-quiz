80539f57b2571c27204adc9accc08fb1eaef4640
YoungJun-L <dudwns328@naver.com>
2024-09-11 17:58:36 +0900

    8-14번까지 추가

--- src/main/kotlin/org/example/quiz/advancedlist/quiz/Quiz8.kt
+++ src/main/kotlin/org/example/quiz/advancedlist/quiz/Quiz8.kt
@@ -1,6 +1,7 @@
 package org.example.quiz.advancedlist.quiz
 
 import org.example.quiz.error.solution.Result
+import org.example.quiz.error.solution.map2
 
 sealed class List<A> {
     abstract fun isEmpty(): Boolean
@@ -19,6 +20,8 @@ sealed class List<A> {
      */
     abstract fun headSafe(): Result<A>
 
+    abstract fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (A) -> (B)): B
+
     /**
      * 연습문제 8-3
      *
@@ -34,12 +37,71 @@ sealed class List<A> {
      */
     fun headSafeV2(): Result<A> = TODO()
 
+    /**
+     * 연습문제 8-11
+     *
+     * 리스트를 묶거나 풀기: [unzip] 함수를 일반화해서 A 타입의 값을 받아 [Pair] 를 반환하는 함수가 인자로 주어지면 List`<`A`>` 타입 리스트를 리스트의
+     * [Pair] 로 변환하는 함수를 정의하라.
+     */
+    fun <A1, A2> unzipV2(f: (A) -> Pair<A1, A2>): Pair<List<A1>, List<A2>> = TODO()
+
+    /**
+     * 연습문제 8-12
+     *
+     * 인덱스로 원소 접근하기: 인덱스를 인자로 받아 인덱스에 해당하는 원소를 돌려주는 함수를 작성하라. 이 함수는 인덱스가 범위를 벗어나도 예외를 던져서는
+     * 안 된다.
+     */
+    fun getAt(index: Int): Result<A> = TODO()
+
+    /**
+     * 연습문제 8-13(어려움)
+     *
+     * 인덱스로 원소 접근하기: 결과를 찾자마자 종료되는 폴드 기반 [getAt] 함수를 구현하라.
+     * > short circuit 을 사용하는 [foldLeft] 를 구현하라.
+     */
+    fun getAtShort(index: Int): Result<A> = TODO()
+
+    /**
+     * 연습문제 8-14
+     *
+     * 리스트 나누기: Int 타입의 값을 파라미터로 받아서 그 값이 가리키는 위치를 중심으로 리스트를 둘로 나누는 함수를 작성하라. 언제든 [IndexOutOfBoundsException]
+     * 이 발생하지 않아야 한다. 대신에 인덱스가 0보다 적은 경우는 0으로 인덱스를 취급하고, 인덱스가 가능한 최댓값을 넘을 때에는 가능한 최댓값으로 취급한다.
+     * > 명시적으로 재귀를 사용하라.
+     */
+    fun splitAt(index: Int): Pair<List<A>, List<A>> = TODO()
+
+    /**
+     * 연습문제 8-15
+     *
+     * 리스트 나누기: 재귀를 사용하는 대신 접기 연산을 사용하여 [splitAt] 을 구현하라.
+     * > [foldLeftShort] 를 이용
+     */
+    fun splitAtV2(index: Int): Pair<List<A>, List<A>> = TODO()
+
+    /**
+     * 연습문제 8-16
+     *
+     * 부분 리스트 검색하기: 한 리스트가 다른 리스트의 부분 리스트인지 검사하는 함수를 구현하라.
+     * > 어떤 리스트가 다른 리스트의 시작 부분에 위치한 부분 리스트인지 검사하는 [startsWith] 함수를 구현한다. 재귀적으로 이 함수를 사용해 구현한다.
+     */
+    fun hasSubList(sub: List<A>): Boolean = TODO()
+    fun startsWith(sub: List<A>): Boolean = TODO()
+
+    /**
+     * 연습문제 8-17
+     *
+     * 리스트를 처리하는 다른 함수들: A에서 B로 가는 함수를 인자로 받고, 이를 각 원소에 적용할 때 반환되는 값을 key, 해당 원소들을 value 로 하는
+     * Map 을 반환하는 groupBy 함수를 만들어라.
+     */
+    fun <B> groupBy(f: (A) -> B): Map<A, List<B>> = TODO()
+
     abstract class Empty<A> : List<A>()
 
     internal object Nil : Empty<Nothing>() {
         override fun isEmpty(): Boolean = true
         override val length: Int = TODO()
         override fun headSafe(): Result<Nothing> = TODO()
+        override fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (Nothing) -> B): B = TODO()
 
         override fun toString(): String = "[NIL]"
     }
@@ -119,7 +181,10 @@ sealed class List<A> {
         val tail: List<A>
     ) : List<A>() {
         override val length = TODO()
+
         override fun headSafe(): Result<A> = TODO()
+
+        override fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (A) -> B): B = TODO()
 
         override fun isEmpty(): Boolean = false
 
@@ -173,8 +238,51 @@ sealed class List<A> {
 /**
  * 연습문제 8-5
  *
- * Result 를 반환하는 리스트 처리하기: List`<`Result`<`A`>>` 를 인자로 받아 성공한 값이 모두 들어 있는 List`<`A`>` 를 반환하는 함수를
- * 정의하라. 실패나 비어 있는 값은 무시한다.
+ * List`<`Result`>` 를 Result`<`List`>` 로 변환하기: List`<`Result`<`A`>>` 를 인자로 받아 성공한 값이 모두 들어 있는 List`<`A`>` 를 반환하는 함수를
+ * 정의하라. 실패나 비어 있는 값은 무시한다. 명시적으로 재귀를 사용하지 말라.
  * > Result 원소가 성공이면 List 를, 아니면 빈 리스트로 만드는 함수를 이용
  */
 fun <A> flattenResult(list: List<Result<A>>): List<A> = TODO()
+
+/**
+ * 연습문제 8-6
+ *
+ * List`<`Result`>` 를 Result`<`List`>` 로 변환하기: List`<`Result`<`A`>>` 를 Result`<`List`<`A`>>` 로 조합하는 함수를 작성하라.
+ * 이 함수는 원래 리스트의 모든 원소가 Success 의 인스턴스일 때만 Success`<`List`<`A`>>` 를 반환하고, 다른 경우에는 항상 Failure`<`List`<`A`>>`
+ * 를 반환한다.
+ * > 재귀를 명시적으로 사용하지 말고 [List.foldRight], [map2] 이용
+ */
+fun <A> sequence(list: List<Result<A>>): Result<List<A>> = TODO()
+
+/**
+ * 연습문제 8-7
+ *
+ * List`<`Result`>` 를 Result`<`List`>` 로 변환하기: A 타입 원소로 이뤄진 리스트를 순회하면서 각 원소에 A에서 Result<`B`>` 로 가는 함수를
+ * 적용해 Result`<`List`<B`>>` 타입을 만드는 보다 일반적인 traverse 함수를 정의하라. traverse 로 새로운 [sequence] 를 정의하라.
+ */
+fun <A, B> traverse(list: List<A>, f: (A) -> Result<B>): Result<List<B>> = TODO()
+fun <A> sequenceV2(list: List<Result<A>>): Result<List<A>> = TODO()
+
+/**
+ * 연습문제 8-8
+ *
+ * 리스트를 묶거나 풀기: 원소 타입이 다른 두 리스트의 원소를 인자로 받은 함수를 통해 원소를 조합해 새로운 리스트를 만드는 함수를 작성하라.
+ * > 두 리스트를 묶을 때 짧은 쪽으로 제약된다.
+ */
+fun <A, B, C> zip(list1: List<A>, list2: List<B>, f: (A) -> (B) -> C): List<C> = TODO()
+
+/**
+ * 연습문제 8-9
+ *
+ * 리스트를 묶거나 풀기: 두 리스트에서 가능한 모든 원소의 조합으로 이뤄진 리스트를 만들어내는 함수를 작성하라.
+ * > 명시적으로 재귀를 쓸 필요가 없다.
+ */
+fun <A, B, C> product(list1: List<A>, list2: List<B>, f: (A) -> (B) -> C): List<C> = TODO()
+
+/**
+ * 연습문제 8-10
+ *
+ * 리스트를 묶거나 풀기: [Pair] 로 이뤄진 리스트를 리스트의 [Pair] 로 변환하는 함수를 작성하라.
+ * > 명시적으로 재귀를 사용하지 말라.
+ */
+fun <A, B> unzip(list: List<Pair<A, B>>): Pair<List<A>, List<B>> = TODO()
--- src/main/kotlin/org/example/quiz/advancedlist/solution/Solution8.kt
+++ src/main/kotlin/org/example/quiz/advancedlist/solution/Solution8.kt
@@ -1,6 +1,10 @@
 package org.example.quiz.advancedlist.solution
 
 import org.example.quiz.error.solution.Result
+import org.example.quiz.error.solution.Result.Companion.failure
+import org.example.quiz.error.solution.map2
+import kotlin.math.max
+import kotlin.math.min
 
 sealed class List<A> {
     abstract fun isEmpty(): Boolean
@@ -9,9 +13,60 @@ sealed class List<A> {
 
     abstract fun headSafe(): Result<A>
 
+    abstract fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (A) -> (B)): B
+
     fun lastSafe(): Result<A> = foldLeft(Result()) { _ -> { e -> Result(e) } }
 
     fun headSafeV2(): Result<A> = foldRight(Result()) { e -> { _ -> Result(e) } }
+
+    fun <A1, A2> unzipV2(f: (A) -> Pair<A1, A2>): Pair<List<A1>, List<A2>> =
+        this.foldRight(Pair(invoke(), invoke())) { e ->
+            { acc ->
+                f(e).let {
+                    Pair(acc.first.cons(it.first), acc.second.cons(it.second))
+                }
+            }
+        }
+
+    fun getAt(index: Int): Result<A> {
+        tailrec fun getAt(list: List<A>, index: Int): Result<A> = when (list) {
+            is Cons -> if (index == 0) Result(list.head) else getAt(list.tail, index - 1)
+            else -> failure("Dead code. Should never execute.")
+        }
+        return if (index < 0 || index >= length)
+            failure("Index out of bound")
+        else
+            getAt(this, index)
+    }
+
+    fun getAtV2(index: Int): Result<A> = (failure<A>("Index out of bound") to index).let {
+        if (index < 0 || index >= length) it
+        else
+            foldLeft(it) { acc ->
+                { e -> if (acc.second < 0) acc else Result(e) to acc.second - 1 }
+            }
+    }.first
+
+    fun getAtShort(index: Int): Result<A> = (failure<A>("Index out of bound") to index).let {
+        if (index < 0 || index >= length) it
+        else
+            foldLeftShort(it, { x -> x.second < 0 }) { acc -> { e -> Result(e) to acc.second - 1 } }
+    }.first
+
+    fun splitAt(index: Int): Pair<List<A>, List<A>> {
+        tailrec fun splitAt(acc: List<A>, list: List<A>, i: Int): Pair<List<A>, List<A>> = when (list) {
+            is Cons -> if (i == 0) list.reverse() to acc else splitAt(acc.cons(list.head), list.tail, i - 1)
+            else -> list.reverse() to acc
+        }
+        return splitAt(invoke(), this.reverse(), length - max(min(index, length), 0))
+    }
+
+    fun splitAtV2(index: Int): Pair<List<A>, List<A>> = TODO()
+
+    fun hasSubList(sub: List<A>): Boolean = TODO()
+    fun startsWith(sub: List<A>): Boolean = TODO()
+
+    fun <B> groupBy(f: (A) -> B): Map<A, List<B>> = TODO()
 
     abstract class Empty<A> : List<A>()
 
@@ -19,6 +74,7 @@ sealed class List<A> {
         override fun isEmpty(): Boolean = true
         override val length: Int = 0
         override fun headSafe(): Result<Nothing> = Result()
+        override fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (Nothing) -> B): B = identity
 
         override fun toString(): String = "[NIL]"
     }
@@ -101,6 +157,14 @@ sealed class List<A> {
 
         override fun headSafe(): Result<A> = Result(head)
 
+        override fun <B> foldLeftShort(identity: B, p: (B) -> Boolean, f: (B) -> (A) -> B): B {
+            tailrec fun foldLeftShort(acc: B, list: List<A>): B = when (list) {
+                is Cons -> if (p(acc)) acc else foldLeftShort(f(acc)(list.head), list.tail)
+                else -> acc
+            }
+            return foldLeftShort(identity, this)
+        }
+
         override fun isEmpty(): Boolean = false
 
         override fun toString(): String = "[${toString("", this)}NIL]"
@@ -152,3 +216,36 @@ sealed class List<A> {
 
 fun <A> flattenResult(list: List<Result<A>>): List<A> =
     list.flatMap { result -> result.map { List(it) }.getOrElse(List()) }
+
+fun <A> sequence(list: List<Result<A>>): Result<List<A>> =
+    list.foldRight(Result(List())) { e ->
+        { acc -> map2(e, acc) { a -> { b -> b.cons(a) } } }
+    }
+
+fun <A, B> traverse(list: List<A>, f: (A) -> Result<B>): Result<List<B>> =
+    list.foldRight(Result(List())) { e ->
+        { acc -> map2(f(e), acc) { a -> { b -> b.cons(a) } } }
+    }
+
+fun <A> sequenceV2(list: List<Result<A>>): Result<List<A>> = traverse(list) { it }
+
+fun <A, B, C> zip(list1: List<A>, list2: List<B>, f: (A) -> (B) -> C): List<C> {
+    tailrec fun zip(list1: List<A>, list2: List<B>, acc: List<C>): List<C> = when (list1) {
+        is List.Cons -> when (list2) {
+            is List.Cons -> zip(list1.tail, list2.tail, acc.cons(f(list1.head)(list2.head)))
+            else -> acc
+        }
+
+        else -> acc
+    }
+    return zip(list1, list2, List()).reverse()
+}
+
+fun <A, B, C> product(list1: List<A>, list2: List<B>, f: (A) -> (B) -> C): List<C> =
+    list1.flatMap { a -> list2.map { b -> f(a)(b) } }
+
+fun <A, B> unzip(list: List<Pair<A, B>>): Pair<List<A>, List<B>> = list.foldRight(Pair(List(), List())) { e ->
+    { acc ->
+        Pair(acc.first.cons(e.first), acc.second.cons(e.second))
+    }
+}
